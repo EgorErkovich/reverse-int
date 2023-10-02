3f409b68b496bef18c3b48c70e198c66dd176b54
egorerkovich <erkovich.egor@yandex.by>
2023-10-02 21:50:59 +0300

    First attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let res = 0;
+    n = Math.abs(n);
+    let i = n.toString().length - 1;
+    while (n / 10 > 0) {
+        res += (n % 10) * 10**i;
+        n = Math.floor(n / 10);
+        i--;
+  }
+  return res;
 }
